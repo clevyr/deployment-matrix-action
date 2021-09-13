d8b7a6271e57063368ef6af0cc0be367925cab54
Gabe Cook <gabe565@gmail.com>
2021-09-13 17:37:14 -0500

    :construction_worker: Remove stash workflow since it was breaking pre-commit lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,11 +2,7 @@ module.exports = {
   "src/*.js": [
     "eslint --fix",
     "prettier --write",
-
-    // Build staged changes
-    () => "git stash push --keep-index --include-untracked :/src",
     () => "npm run build",
     () => "git add :/dist",
-    () => "git stash drop",
   ],
 };
